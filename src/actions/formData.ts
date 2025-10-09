2da4b4ad5c2a5201f9fe497578e52801b7593b59
ArtemAndr <artem.andrianov.98@gmail.com>
2025-10-09 21:37:05 +0700

    feat: click to form

--- src/actions/formData.ts
+++ src/actions/formData.ts
@@ -6,11 +6,21 @@ const GOOGLE_FORM_URL =
 export const submitForm = defineAction({
   accept: "form",
   handler: async (formData) => {
+    const now = new Date()
+    const dateString = now.toLocaleString("ru-RU", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    })
+
     const payload = new URLSearchParams({
       "entry.1332314686": String(formData.get("name") ?? ""),
       "entry.1506805712": String(formData.get("phone") ?? ""),
       "entry.1790986620": String(formData.get("email") ?? ""),
       "entry.1358437002": String(formData.get("tariff") ?? ""),
+      "entry.1251963195": dateString,
     })
 
     const res = await fetch(GOOGLE_FORM_URL, {
